05cc063343b588a9b8859c9ab303eadf9b254762
Reiya Asuke <rask0x18@gmail.com>
2024-12-14 00:03:19 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
-from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
-
-
-class CustomBuildExt(build_ext):
-    def build_extensions(self):
-        pass
-
+from setuptools import setup
 
 setup(
     name="minipy",
-    version="0.1.0",
-    author="Your Name",
-    description="A minimal numpy-like library with CUDA support",
     packages=["minipy"],
     package_dir={"": "src"},
-    ext_modules=[
-        Extension(
-            "minipy.backend.cuda_ops",
-            ["src/minipy/backend/cuda_ops.cu"],
-        )
-    ],
-    cmdclass={"build_ext": CustomBuildExt},
-    python_requires=">=3.12",
 )
